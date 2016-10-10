86f4f87d1d0fd3832199aa97076d63917369ec7c
boyboi86 <boyboi86@gmail.com>
2016-10-10 19:12:49 +0800

    created main server file

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const sequelize = require('sequelize');
+const cors = require('cors');
+const http = require('http');
+const sequelize = require('sequelize');
+
+const db = require('./models/index');
+const users = require('./routes/users');
+const entries = require('./routes/entries');
+
+const port = process.env.PORT || 3000;
+const config = require('./config');
+
+const app = express();
+const accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
+app.use(logger('combined', { stream: accessLogStream }));
+app.use(bodyParser.json({ type: '*/*' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/users', users);
+app.use('/api/entries', entries);
+
+/*Route reserved for all static assets must be last route to run */
+// app.get('*', function(req, res){
+//   res.sendFile(path.resolve(__dirname, './public/index.html'))
+// });
+
+/*Make life easier but less detailed.. runs server*/
+db.sequelize.sync({force: true})
+  .then(function() {
+	http.createServer(app).listen(port, function() {
+		console.log(`Express listening on port ${port}!`);
+	});
+});
