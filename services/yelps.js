c5cbce3b788f2c346d4de27fd8bedd45fa9034f2
boyboi86 <boyboi86@gmail.com>
2016-10-14 00:10:36 +0800

    api call with yelps package

---
+++ services/yelps.js
@@ -0,0 +1,12 @@
+const yelp = require("yelp");
+
+/*YELPS API CALL*/
+const client = new yelp({
+    "consumer_key": "TYRpd6c_V73BWeeA9-i6Nw",
+    "consumer_secret": "k9BUPw2sByv0l_v68yOitP0Vg_I",
+    "token": "iCuBkoUd_1HC4gM8lXjILHDNreeCRN8R",
+    "token_secret": "VN6_yjq1YwO7OAxPiOTOkw8tHlM"
+});
+
+
+module.exports = client;
